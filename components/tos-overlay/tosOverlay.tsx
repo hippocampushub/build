c718957d62294fa310df359aad2fc72ed6195f59
antonino-tocco <entonytocco@gmail.com>
2021-03-17 19:48:08 +0100

    fix for tos

--- components/tos-overlay/tosOverlay.tsx
+++ components/tos-overlay/tosOverlay.tsx
@@ -6,15 +6,17 @@ import {SanitizedHtml} from "../sanitizedHtml";
 export function TosOverlay({tos, agreeTos}) {
 
     return (<div className={tosStyle['tos-overlay-container']}>
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-12'>
-                    <SanitizedHtml content={tos?.content ?? ''} style={{fontSize: 26}}/>
-                </div>
-                <div className='col-12 text-right' style={{position: 'absolute', bottom: 20}}>
-                    <CustomButton style={{margin: '0 auto'}} onClick={agreeTos}>
-                        Agree
-                    </CustomButton>
+        <div className={tosStyle['tos-overlay-main-container']}>
+            <div className='container-fluid'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <SanitizedHtml content={tos?.content ?? ''} style={{fontSize: 26}}/>
+                    </div>
+                    <div className='col-12 text-right' style={{marginTop: 50}}>
+                        <CustomButton style={{margin: '0 auto'}} onClick={agreeTos}>
+                            I Agree
+                        </CustomButton>
+                    </div>
                 </div>
             </div>
         </div>
