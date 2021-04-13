3075ba38c2df51665cf53ff8f04eb9880bc9cf9f
antonino-tocco <entonytocco@gmail.com>
2021-04-14 00:21:17 +0200

    fix for search bar button

--- components/customSearchBar/index.tsx
+++ components/customSearchBar/index.tsx
@@ -1,13 +1,14 @@
-import SearchBar, {SearchBarProps} from "material-ui-search-bar";
 import * as React from "react";
+import SearchBar, {SearchBarProps} from "material-ui-search-bar";
 import {CustomButton} from "../buttons/buttons";
+import {Search as IconSearch} from "@material-ui/icons";
 import constants from "../../constants";
 
 interface ICustomSearchBarProps extends SearchBarProps {}
 
 export function CustomSearchBar(props: ICustomSearchBarProps) {
     const showButton = props?.value?.trim()?.length >= constants.MIN_SEARCH_LENGTH;
-    const searchBarContainerClassName = showButton ? 'col-9' : 'col-12';
+    const searchBarContainerClassName = showButton ? 'col-md-10 col-sm-12' : 'col-12';
     return (<div className='row'>
         <div className={searchBarContainerClassName}>
             <SearchBar
@@ -18,9 +19,9 @@ export function CustomSearchBar(props: ICustomSearchBarProps) {
             </SearchBar>
         </div>
         {showButton ?
-            <div className='col-3'>
+            <div className='col-md-2 col-sm-12'>
                 <CustomButton onClick={() => props?.onRequestSearch()} style={{fontSize: 16}}>
-                    Search
+                    <IconSearch/>
                 </CustomButton>
             </div> : null
         }
