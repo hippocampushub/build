5172ef019bba39bc32d73c2bc5655054748a1e3b
Antonino Tocco <entonytocco@gmail.com>
2024-01-26 14:14:07 +0100

    fix for hhnb url

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   env: {
     BASE_URL: isProd ? '/build' : '',
     BACKEND_URL: isProd ? 'https://hh-build.ebrains-italy.eu' : 'https://hh-build.ebrains-italy.eu',
-    HODGKIN_HUXLEY_BASE_URL: 'https://hbp-bsp-hhnb.cineca.it/hh-neuron-builder',
+    HODGKIN_HUXLEY_BASE_URL: 'https://hhnb.ebrains-italy.eu/hh-neuron-builder/',
     NEXT_PUBLIC_FEEDBACK_URL: 'https://feedback-proxy-bsp-epfl.apps.hbp.eu',
     SUGGESTION_MIN_LENGTH: 1
   },
